217fe26df32760d7a6c347a423ee6e8936c6305c
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2018-05-23 21:19:17 -0400

    K5

---
+++ Gizmo/synth/KawaiK5.h
@@ -0,0 +1,20 @@
+////// Copyright 2017 by Sean Luke
+////// Licensed under the Apache 2.0 License
+
+
+
+#ifndef __SYNTH_KAWAI_K5_H__
+#define __SYNTH_KAWAI_K5_H__
+
+
+#include "../TopLevel.h"
+#include <Arduino.h>
+
+struct _kawaiK5Local
+	{
+	};
+
+void stateSynthKawaiK5();
+
+#endif
+
